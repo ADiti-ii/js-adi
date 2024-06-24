e6a2053f5a5324fa7456300034e6d7ec04c8269a
Aditi <140682084+ADiti-ii@users.noreply.github.com>
2024-06-24 07:42:50 +0000

    config done

---
+++ 01basics/lcv.js
@@ -0,0 +1,18 @@
+const accountId=13445
+let accountEmail="ab@gmail.com"
+var accountPass="5656"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2 not possible bcz const cant be changed
+
+accountEmail="adi@gmail.com"
+accountPass="4243"
+accountCity="mirzapur"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPass,accountCity])
+/*we will use let only not  var beacause of scope problem 
+scope is{} var use krne se sara data hr jgh update ho jaega*/
+
+console.log(accountState)
