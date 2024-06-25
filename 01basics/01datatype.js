9a8e4aba002b2c015870b4d62694958995959190
Aditi <140682084+ADiti-ii@users.noreply.github.com>
2024-06-25 16:50:41 +0000

    its done

---
+++ 01basics/01datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // this line means it treat all js code as newer version
+// alert(3+3)// shows error bcz we r using node js not browser.
+
+/* Primitive datatypes;-
+1. String => ""
+2.bigint - used for higher numbr ike in trading 
+3.number- 2 to power 53 approx
+4.boolean - true/false
+5.null- standalone value
+6. undefined - value isn't assigned yet
+7.symbol- defines uniqueness
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
